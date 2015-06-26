a2f1d6ed91abc13e2980ced1a2a36995ca11e2af
Ferdinando <Ferdinando.Giordano@cern.ch>
2015-06-26 10:00:51 -0500

    Remove duplicated directory

--- interface/ZmassFromLikelihoodFit.h
+++
@@ -1,7 +0,0 @@
-#ifndef ZmassFromLikelihoodFit_H
-#define ZmassFromLikelihoodFit_H 1
-
-void ZmassFromLikelihoodFit(void);
-int  ZmassFromLikelihoodFit(TString channel, TString cut, std::vector<float> &result);
-
-#endif
